8df3f81ef35b5d546ad72826148f17286c339e1c
kdahun <101082485+kdahun@users.noreply.github.com>
2023-06-02 23:00:28 +0900

    쇼핑평가

---
+++ shopping.py
@@ -0,0 +1,5 @@
+import numpy as nmp
+from keras.layers import Embedding, Flatten, Dense
+from keras.models import Sequential
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
